ea5fff85c42182b842c0eeac9966486f7d981696
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2025-11-18 14:00:51 +0300

    Build fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import {StyledEngineProvider} from '@mui/material/styles';
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import {LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import type {} from '@mui/material/themeCssVarsAugmentation';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
     <StyledEngineProvider injectFirst>
         <BrowserRouter>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <App/>
-            </LocalizationProvider>
+            <App/>
         </BrowserRouter>
     </StyledEngineProvider>
 );
